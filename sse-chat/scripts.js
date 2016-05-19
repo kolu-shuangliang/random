4caa55eeb0173877df4296de26ba602d305b7a30
shuangliang kolu <shuangkolu@hotmail.com>
2016-05-19 18:34:28 +0300

    sse-chat. replace login-form with input-form on login-form submit event.

--- sse-chat/scripts.js
+++ sse-chat/scripts.js
@@ -4,13 +4,58 @@ console.log( 'Server-Sent Events' );
 // GLOBAL VARIABLES
 var current = 0;
 
-
-document.getElementById( 'login-form' ).addEventListener( 'submit', function( event ){
+if( sessionStorage.username ){
+    console.log( 'logged in - username: ' + sessionStorage.username );
     
-    event.preventDefault();
-    console.log( 'login' );
-    
-} );
+    chatinput();
+}
+else{
+    document.getElementById( 'login-form' ).addEventListener( 'submit', function( event ){
+        event.preventDefault();
+        
+        // Checks if 'username' input is empty.
+        if( this.username.value ){
+            console.log( 'logged in - username: ' + this.username.value );
+            // Sets JavaScript session storage.
+            sessionStorage.username = this.username.value;
+            
+            chatinput();
+        }
+        else{
+            alert( 'Insert username!' );
+        }
+    } );
+}
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 // FUNCTIONS
+
+// Replace login-form with chat-form
+function chatinput(){
+    var element = document.getElementById( 'chat-input' );
+    element.innerHTML = '';
+    
+    var form = document.createElement( 'FORM' );
+    form.id = 'chat-form';
+    element.appendChild( form );
+    
+    var input = document.createElement( 'INPUT' );
+    form.appendChild( input );
+    input.name = 'message';
+    input.type = 'TEXT';
+    input.placeholder = 'Logged in as: ' + sessionStorage.username + ' - Enter message.';
+    input.autocomplete = 'off';
+    
+    var button = document.createElement( 'BUTTON' );
+    form.appendChild( button );
+    button.type = 'SUBMIT';
+    button.textContent = 'Send';
+    
+    form.addEventListener( 'submit', function( event ){
+        
+        
+        
+    } );
+    
+    input.focus();
+}
